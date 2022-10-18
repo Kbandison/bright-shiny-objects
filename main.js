dbbb93b17698c19863fc422bb1485b4001d49b09
Kevin Bandison <kbandison@gmail.com>
2022-10-18 10:08:34 -0400

    completed assignment

--- main.js
+++ main.js
@@ -25,8 +25,57 @@ function fixCar(car){
   return car;
 }
 
+function addGrades(student, newGrades){
+  student.grades = student.grades.concat(newGrades);
+  return student;
+}
 
+function getDataType(car, key){
+  let type = key;
 
+  type = typeof car[key];
+  return type;
+}
+
+function addTodo(items, newItem){
+  items.push(newItem);
+  return items;
+}
+
+function addSong(playlist, newSong){
+  playlist.songs.push(newSong);
+  playlist["duration"] = (playlist["duration"] += newSong["duration"]);
+  return playlist;
+}
+
+function updateReportCard(report, newGrade){
+  report.grades.push(newGrade);
+  let highGrade = report.grades[0];
+  let lowGrade = report.grades[0];
+  let total = 0;
+
+  for(let i = 0; i < report.grades.length; i++){
+    total += report.grades[i];
+    if (report.grades[i] < lowGrade && report.grades[i] < report.grades[i + 1]){
+      lowGrade = report.grades[i];
+    } else if (report.grades[i + 1] < lowGrade && report.grades[i] > report.grades[i + 1]){
+      lowGrade = report.grades[i + 1];
+    } else if (report.grades[i] > highGrade && report.grades[i] > report.grades[i + 1]){
+      highGrade = report.grades[i];
+    } else if (report.grades[i + 1] > highGrade && report.grades[i] < report.grades[i + 1]){
+      highGrade = report.grades[i + 1];
+    }
+  }
+
+  let average = total / (report.grades.length);
+  average = Number((Math.round(average * 2) / 2).toFixed(1));
+
+  report.lowestGrade = lowGrade;
+  report.highestGrade = highGrade;
+  report.averageGrade = average
+
+  return report;
+}
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
